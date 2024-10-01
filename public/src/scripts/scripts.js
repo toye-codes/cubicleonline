7b0265fe2e52cf6a7a2b2331b7abcafaf04e4e51
Agbesanya Olusanya <agbesanyaolusanya@gmail.com>
2024-10-01 16:36:04 +0100

    Rearranged the js file

---
+++ public/src/scripts/scripts.js
@@ -0,0 +1,7 @@
+
+const navToggle = document.getElementById("navToggle");
+const navLinks = document.getElementById("navLinks");
+
+    navToggle.addEventListener("click", () => {
+        navLinks.classList.toggle("active")
+    } )
